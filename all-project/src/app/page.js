16937993f92f695b271285c1e3ee47e464d32db9
sohaila-aliiiiii <asohaila632@gmail.com>
2024-10-19 22:40:57 +0300

    best saller

--- all-project/src/app/page.js
+++ all-project/src/app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Header from "./_components/Header/Header";
+import Best from "./_components/BestSaler/page";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
     <div >
       <Header/>
+      <Best/>
     </div>
   );
 }
