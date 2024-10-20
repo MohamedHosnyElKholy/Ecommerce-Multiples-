e178ecb767095a380824b2d9e975ad7cb351b54b
Mohamed <mamedhosny345@gmail.com>
2024-10-20 16:32:53 +0300

    تحديثات متعددة

--- all-project/src/app/_components/BestSaler/page.jsx
+++ all-project/src/app/_components/BestSaler/page.jsx
@@ -1,85 +1,92 @@
-'use client'
-import React from 'react' 
-// import img1 from '../assets/s1.jpg' 
-import { FaStar } from "react-icons/fa6"; 
- 
-import Slider from "react-slick"; 
-import Image from 'next/image'
- import img1 from '@/images/s1.jpg'
-export default function Best() { 
-    var settings = { 
-        dots: true, 
-        speed: 500, 
-        slidesToShow: 3, 
-        slidesToScroll: 2, 
-        arrows:false, 
-        autoplaySpeed:500, 
-        autoplay:true, 
-        infinite: false, 
-        initialSlide: 0, 
-        focusOnSelect: true, 
-         
-        responsive: [ 
-        
-            { 
-              breakpoint: 600, 
-              settings: { 
-                slidesToShow: 2, 
-                slidesToScroll: 2, 
-                initialSlide: 2 
-              } 
-            }, 
-            { 
-              breakpoint: 480, 
-              settings: { 
-                slidesToShow: 1, 
-                slidesToScroll: 1 
-              } 
-            } 
-          ], 
-      
- 
-      }; 
-      const products=[0,1,2,3,4,5,6,7,8] 
-  return ( 
-    <> 
-      <div className='best container mx-auto  p-10 '> 
-      <ul className='flex space-x-6 md:space-x-20 mx-auto justify-center text-[16px] md:text-[20px] text-[#9892a6] font-bold'>
+"use client";
+import React from "react";
+// import img1 from '../assets/s1.jpg'
+import { FaStar } from "react-icons/fa6";
+
+import Slider from "react-slick";
+import Image from "next/image";
+import img1 from "@/images/s1.jpg";
+import { Button } from "flowbite-react";
+
+export default function Best() {
+  var settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    arrows: false,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    infinite: false,
+    initialSlide: 0,
+    focusOnSelect: true,
+
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const products = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  return (
+    <>
+      <div className=" container mx-auto px-4 py-8">
+        <ul className="flex space-x-6 md:space-x-20 mx-auto justify-center text-[16px] md:text-[20px] text-[#9892a6] font-bold">
           <li>LG</li>
           <li>SONY</li>
           <li>SAMSUNG</li>
           <li>TOSHIBA</li>
         </ul>
-      <h1 className='p-2 md:text-[30px] text-[20px]  mx-auto  '>Best Saller</h1>
-      <Slider {...settings}> 
-      {products.map((p,i)=> 
-           <div className='w-1/4 p-2'>     
-             <div  className="card p-2  py-3  "> 
-             {/* <img src={img1} className='w-1/2 mx-auto'/>  */}
-             <Image src={img1} width={500} height={500} className='w-1/2 mx-auto' alt="Picture of the author"/>
-             <h1>Lorem ipsum dolor sit amet Lorem, ipsum </h1> 
-             <div className='flex justify-between my-1'> 
-            <span>price:</span> 
-            <span>5000$</span> 
-             </div> 
-            <div className='flex justify-between my-1'> 
-            <span className='flex text-[#ff9900] space-x-2'> <FaStar /><FaStar /><FaStar /> <FaStar /></span> 
-            <span>500</span> 
-            </div> 
-        
-           <button className='bg-[#ff9900] text-white w-full rounded-sm mt-2' > Add to cart</button> 
- 
- 
-           </div> 
-        </div> 
-       
-        )} 
-      </Slider> 
-        
-     
- 
-      </div> 
-       
-    </> 
-  ) 
+        <h1 className="p-2 md:text-[40px] text-[20px]  mx-auto font-semibold  ">
+          Best Saller
+        </h1>
+        <Slider {...settings}>
+          {products.map((p, i) => (
+            <div className="w-1/4 p-2">
+              <div className="card p-2  py-3  ">
+                <Image
+                  src={img1}
+                  width={232}
+                  height={232}
+                  className="w-1/2 mx-auto"
+                  alt="Picture of the author"
+                />
+                <p className="text-[22px] text-black mt-3">
+                Lorem ipsum dolor sit amet
+              </p>
+              <div className="flex justify-between mt-3">
+                <p className="text=[24px] text-black">Price:</p>
+                <p className="text=[24px] text-black">5000$</p>
+              </div>
+                <div className="flex justify-between my-1">
+                  <span className="flex text-[#ff9900] space-x-2">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar /> <FaStar />
+                  </span>
+                  <span>500</span>
+                </div>
+
+                <Button className="pt-[10px] pb-[10px] text-[#fff] bg-[#ff9900] w-[100%] mt-3">
+                Add To Cart
+              </Button>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </>
+  );
 }
--- all-project/src/app/_components/Header/Header.jsx
+++ all-project/src/app/_components/Header/Header.jsx
@@ -1,20 +1,36 @@
-import Image from 'next/image'
-import React from 'react'
-import heroImg from '@/images/imgForHeroSection.png'
+import Image from "next/image";
+import React from "react";
+import heroImg from "@/images/imgForHeroSection.png";
 export default function Header() {
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 px-10 h-[auto] bg-[#9BFFF84D] pt-[156px] '>
-        <div className=' my-auto'>
-            <div className='max-w-[419px] mx-auto block'>
-            <h1 className='xl:text-[72px] text-[42px] font-[500]  drop-shadow-[0_4px_4px_#00000040]'>ONE CART</h1>
-            <p className='my-[48px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam molestiae nulla sed voluptatem dolores, deleniti perferendis, ad error non illo, cum soluta id facere impedit? Iste eius magnam aliquid facilis.</p>
-            <button type="button" class="focus:outline-none text-white  bg-[#FF9900] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-bold rounded-[4px] text-sm px-[24px] py-[16px] me-2 mb-2">SHOP NOW</button>
+    <div className="grid grid-cols-1 xl:grid-cols-2 px-10 h-[auto] bg-[#9BFFF84D] pt-[156px] ">
+      <div className=" my-auto">
+        <div className="max-w-[419px] mx-auto block">
+          <h1 className="xl:text-[72px] text-[42px] font-[500]  drop-shadow-[0_4px_4px_#00000040]">
+            ONE CART
+          </h1>
+          <p className="my-[48px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            molestiae nulla sed voluptatem dolores, deleniti perferendis, ad
+            error non illo, cum soluta id facere impedit? Iste eius magnam
+            aliquid facilis.
+          </p>
+          <button
+            type="button"
+            class="focus:outline-none text-white  bg-[#FF9900] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-bold rounded-[4px] text-sm px-[24px] py-[16px] me-2 mb-2"
+          >
+            SHOP NOW
+          </button>
         </div>
-
-            </div>
-        <div className='my-auto'>
-            <Image src={heroImg} width={589} height={732} className='block mx-auto'></Image>
-        </div>
+      </div>
+      <div className="my-auto">
+        <Image
+          src={heroImg}
+          width={589}
+          height={732}
+          className="block mx-auto"
+        ></Image>
+      </div>
     </div>
-  )
+  );
 }
