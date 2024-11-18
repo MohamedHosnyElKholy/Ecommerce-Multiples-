304d8ae456083764a61b1c71abf909a9532423e3
gemy3731 <mohamedgamalz3731@gmail.com>
2024-11-18 23:56:57 +0200

    feat(auth): add checkEmail page

---
+++ all-project/src/app/(page)/(forgetPassword)/checkEmail/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CheckEmail from './../../../_components/checkEmail/CheckEmail';
+
+export default function page() {
+  return (
+    <div>
+      <CheckEmail/>
+    </div>
+  )
+}
